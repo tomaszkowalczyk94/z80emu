d55734fb2a6c045e9e82d4ff6b375cfb5ddfd309
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2018-12-03 14:17:49 +0100

    clear project I was decide change all project. Now wont emulate all modules, but just instruction. Now it will be more interpreter than emulator

--- src/main/java/org/tomaszkowalczyk94/z80emu/core/App.java
+++ src/main/java/org/tomaszkowalczyk94/z80emu/core/App.java
@@ -1,11 +1,12 @@
-package edu.psk.z80emu;
+package org.tomaszkowalczyk94.z80emu.core;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ src/test/java/org/tomaszkowalczyk94/z80emu/core/AppTest.java
@@ -0,0 +1,12 @@
+package org.tomaszkowalczyk94.z80emu.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void main() {
+    }
+}
